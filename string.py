53c7d5d99752853f49dba3399551acad76afba25
soyoung <soyoungp02@gmail.com>
2019-08-11 19:33:33 +0900

    데이터 타입 - 문자열 연습

---
+++ string.py
@@ -0,0 +1,22 @@
+a = 'Hello Python'
+print(a)
+
+#length
+print(len(a))
+
+#index
+print(a[0])
+print(a[1])
+print(a[2:5])
+
+#repeat
+print((a+'\n')*2)
+
+#docstring
+print('''
+h
+e
+l
+l
+o
+''')
---
+++ string2.py
@@ -0,0 +1,5 @@
+#positional formatting
+print('to {}, loremElit est adipisicing amet non. In eiusmod enim id {} veniam do sint veniam aute adipisicing non exercitation Lorem. Quis irure eu id nostrud esse in dolor proident eu irure. {} Laborum duis pariatur exercitation ipsum officia consectetur incididunt {} id ad cillum incididunt eiusmod anim.'.format('soyoung', 12, 'soyoung', 'soyoung'))
+
+#Named placeholder
+print('to {name}, loremElit est adipisicing amet non. In eiusmod enim id {age} veniam do sint veniam aute adipisicing non exercitation Lorem. Quis irure eu id nostrud esse in dolor proident eu irure. {name} Laborum duis pariatur exercitation ipsum officia consectetur incididunt {name} id ad cillum incididunt eiusmod anim.'.format(name='soyoung', age='29'))
